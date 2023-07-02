7d610e8a4ec33c9f9381ee74e089ad51cb66e960
Guilherme <gui-teixeirapimentel@hotmail.com>
2023-07-02 12:37:57 -0300

    Specialized to string on bool to write true or false

--- src/lox/LiteralUtils.cpp
+++ src/lox/LiteralUtils.cpp
@@ -6,6 +6,7 @@ std::string pimentel::literalToString(Token::LiteralType literal)
     return std::visit(overloaded{
         [](const std::string& arg) { return arg; },
         [](void* unused_) { (void)unused_; return std::string{"NULL"}; },
+        [](const bool& arg) { return std::string{arg ? "true" : "false"}; },
         [](const auto& arg) { return std::to_string(arg); },
     }, literal);
 }
